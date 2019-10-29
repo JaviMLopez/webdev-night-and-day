cf7a5aedbf1712427b5827554cd984d3ffc85ebc
Javier Lopez <jlopez20203@columbushs.com>
2019-10-29 18:59:24 -0400

    Needs Tuning

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+    $('#orb').click(function () {
+        if ($(this).hasClass('sun')) {
+            $(this).removeClass('sun').addClass('moon');
+        }
+        else {
+            $(this).removeClass('moon').addClass('sun');
+        }
+        if ($('#sky').hasClass('day')) {
+            $('#sky').removeClass('day').addClass('night');
+        }
+        else {
+            $('#sky').removeClass('night').addClass('day');
+        }
+        if ($('#moonspot1').hasClass('visible')) {
+            $('#moonspot1').removeClass('visible');
+        } else {
+            $('#moonspot1').addClass('visible');
+        }
+    });
+});
